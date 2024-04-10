8d170505acf660853a4d1b706aa21d1bbef23303
Colin Ciervo <colinciervo@@gmail.com>
2024-04-10 12:32:37 -0400

    Add photos

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import bandPic from '../../Assets/fjordponybandphoto2.jpg'
+import amythyst from '../../Assets/amythysts.png'
+import brights from '../../Assets/brights-on.jpeg'
 
 function App() {
   return (
@@ -9,6 +11,11 @@ function App() {
         <img className='band-pic' src={bandPic} />
         <p className='description'>Fjord Pony is an indie rock band whose homebase is Middletown, Connecticut. When they roll up their sleeves and dive into the DIY recording process at their studio clubhouse, it's all about the songwriting—no gimmicks, no tricks. Summoning the ghosts of pop, post-punk, British Invasion, prog rock, alt-country, and the California sound, the band channels their distinct and intersecting influences into a halcyon mashup. Fjord Pony is songwriter Chris Wiedemann (vocals/guitar, left center), Rick Ciervo (keys/guitar, right center), Mike Mongillo (bass guitar, left), and Jesse Alford (drums/percussion, right). And in case you were wondering, no, the name Fjord Pony does not have a satisfying backstory.
         </p>
+        <div>
+        <h3>Releases:</h3>
+        <img className='brights' src={brights} />
+        <img className='amythyst' src={amythyst} />
+        </div>
       </div>
     </div>
   )
